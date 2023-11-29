b0c7025b1539f71d2a9fc860fc43c460ad96ce4a
sanr_w <fieldstreet.27@gmail.com>
2023-11-29 14:43:49 +0300

    added some templates

--- app.py
+++ app.py
@@ -1,35 +1,3 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'free JPEG to PDF online converter'
-#
-#
-# if __name__ == '__main__':
-#     app.run()
-#
-
-
-# from flask import Flask
-# from gevent.pywsgi import WSGIServer
-#
-# app = Flask(__name__)
-#
-# @app.route('/api', methods=['GET'])
-# def index():
-#     return "free JPEG to PDF online converter"
-#
-# if __name__ == '__main__':
-#     # Debug/Development
-#     # app.run(debug=True, host="0.0.0.0", port="5000")
-#     # Production
-#     http_server = WSGIServer(('', 5000), app)
-#     http_server.serve_forever()
-
-
 from main import app
 
 
--- main.py
+++ main.py
@@ -3,19 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "<h1>free JPEG to PDF online converter</h1>"
-
-
-@app.route('/startpage', methods=['GET'])
-def startpage():
-    form = request.form['file']
-
-    return render_template('startpage.html', form=form)
-
-
-@app.route('/hello')
-def hello():
     return render_template('hello.html')
 
