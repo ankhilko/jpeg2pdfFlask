c7f4f3e73a134b8f9a1210c7327dccc5ea374ea9
sanr_w <fieldstreet.27@gmail.com>
2023-12-01 20:54:57 +0300

    working on part 6

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
+import smtplib
+
 import img2pdf
 from flask import Flask, render_template, request
 from datetime import datetime
 
 
 app = Flask(__name__)
+
+subscribers = []
 
 
 def date_turn():
@@ -43,6 +47,42 @@ def converter_func(jpeg_path=os.getcwd(), output_path=os.getcwd(), file_name='fi
 def index():
     title = 'JPG to PDF converter'
     return render_template('hello.html', title=title)
+
+
+@app.route('/subcribe')
+def subcribe():
+    title = 'JPG to PDF converter'
+    return render_template('subcribe.html', title=title)
+
+
+@app.route('/form', methods=['POST'])
+def form():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    email = request.form.get('email')
+    # email = request.form['email']
+
+    message = 'You have subscribed to my newsletter. Thank you!'
+    server = smtplib.SMTP('smtp.gmail.com', port=587)
+    server.starttls()
+    server.login('sanrkhilko@gmail.com', 'ICH_will_ficken_2013')
+    server.sendmail('Alex', email, message)
+
+    if not first_name or not last_name or not email:
+        error_state = 'All fields are required...'
+        return render_template('subcribe.html', error_state=error_state,
+                               first_name=first_name,
+                               last_name=last_name,
+                               email=email)
+
+    subscribers.append(f'{first_name} {last_name}, {email}')
+
+    print(first_name)
+    print(last_name)
+    print(email)
+
+    title = 'Thank you!'
+    return render_template('form.html', title=title, subscribers=subscribers)
 
 
 @app.route('/about')
