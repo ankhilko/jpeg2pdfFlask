e85ea17bba1c9ca6e50203dfe085046c3b3954cb
sanr_w <fieldstreet.27@gmail.com>
2023-12-12 15:44:37 +0300

    converter output is the same name as imported file

--- converter.py
+++ converter.py
@@ -1,16 +1,17 @@
 
-def convert_j2p():
+def convert_j2p(png_img):
     import jpg2pdf
-    with jpg2pdf.create('me.pdf') as new_pdf:
-        new_pdf.add('me.jpg')
+    with jpg2pdf.create(f'{png_img[:-3]}pdf') as new_pdf:
+        new_pdf.add(png_img)
 
 
 def conv_p_j(png_img):
     from PIL import Image
     new_img = Image.open(png_img)
-    new_img.save('me.jpg')
+    new_img.save(f'{png_img[:-3]}jpg')
 
 
 if __name__ == '__main__':
-    convert_j2p()
+
+    pass
 
